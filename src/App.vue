b8ab26fc7aa4f40360f4819c910b2c931f355926
sophia656 <antropovasophia@gmail.com>
2022-04-02 18:09:40 +0300

    changePage

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,19 @@
         v-if="!isPostsLoading"
         />
         <div v-else>LOADING...</div>
+        <div class="page__wrapper">
+            <div 
+            v-for="pageNumber in totalPages" 
+            :key="pageNumber"
+            class="page"
+            :class="{
+                'current-page': page === pageNumber
+            }"
+            @click="changePage(pageNumber)"
+            >
+                {{ pageNumber }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -50,6 +63,9 @@ export default {
             isPostsLoading: false,
             selectedSort: '',
             searchQuery: '',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По описанию'}
@@ -67,10 +83,20 @@ export default {
         showDialog() {
             this.dialogVisible = true
         },
+        changePage(pageNumber) {
+            this.page = pageNumber
+            this.fetchPosts()
+        },
         async fetchPosts() {
             try {
                 this.isPostsLoading = true;
-                    const responce = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                    const responce = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit
+                        }
+                    });
+                    this.totalPages = Math.ceil(responce.headers['x-total-count'] / this.limit)
                     this.posts = responce.data;
             } catch (e) {
                 alert('ERROR')
@@ -113,5 +139,19 @@ export default {
     margin: 15px 0;
     display: flex;
     justify-content: space-between;
+}
+.page__wrapper {
+    display: flex;
+    margin-top: 15px;
+    width: 100%;
+    cursor: pointer;
+}
+.page {
+    border: 1px solid black;
+    padding: 10px;
+    margin: 0 5px;
+}
+.current-page {
+    border: 2px solid rgb(212, 110, 152);
 }
 </style>
