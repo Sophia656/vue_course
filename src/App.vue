ab2a3ed14ad42da790ba77d3741f716af7e0478a
sophia656 <antropovasophia@gmail.com>
2022-04-02 15:09:17 +0300

    dialog form -- ui

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 
 <template>
     <div class="app">
-        <post-form
-        @create="createPost"
-        />
+        <h1>Страница с постами</h1>
+        <my-button
+        @click="showDialog"
+        >
+            Создать пост
+        </my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form
+                @create="createPost"
+            />
+        </my-dialog>
         <post-list 
         :posts="posts"
         @remove="removePost"
@@ -14,9 +22,13 @@
 <script>
 import PostForm from '@/components/PostForm.vue';
 import PostList from '@/components/PostList.vue';
+import MyDialog from './components/UI/MyDialog.vue';
+import MyButton from './components/UI/MyButton.vue';
 export default {
     components: {
-        PostList, PostForm
+        PostList, PostForm,
+        MyDialog,
+        MyButton
     },
     data() {
         return {
@@ -25,14 +37,19 @@ export default {
                 {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
                 {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'}
             ],
+            dialogVisible: false,
         }
     },
     methods: {
         createPost(post) {
-            this.posts.push(post)
+            this.posts.push(post);
+            this.dialogVisible = false;
         },
         removePost(post) {
             this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog() {
+            this.dialogVisible = true
         }
         // inputTitle(event) {
         //     this.title = event.target.value
